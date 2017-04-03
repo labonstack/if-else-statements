4e58ea59277669eed251b0c3ed8f539d673daad6
Khodidas Diyora <khodidasdiyora@gmail.com>
2017-04-03 12:28:34 +0530

    Add files via upload

---
+++ if-else-statements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is an example of the if and if-else statements */
+void main( ) 
+{ 
+    int data;
+    for(data = 0;data < 10;data = data + 1) 
+    {
+      if (data == 2)
+       printf("Data is now equal to %d\n",data);
+    }
+}
